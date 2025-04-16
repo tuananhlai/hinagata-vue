91403846c48c9d3705686b7c3a0174564fe31bf2
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2025-04-16 20:13:56 +0900

    minor changes

--- packages/core/tests/Tooltip.test.ts
+++ packages/core/tests/Tooltip.test.ts
@@ -3,6 +3,37 @@ import { expect, it } from "vitest";
 import { h, nextTick } from "vue";
 import { Provider, Tooltip, TooltipContent, TooltipTrigger } from "../src";
 
+it("should have the correct role", async () => {
+  render(
+    h({
+      template: `
+    <Provider>
+      <Tooltip default-open>
+        <TooltipTrigger as-child>
+          <button>Hover me</button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Tooltip Content</p>
+        </TooltipContent>
+      </Tooltip>
+    </Provider>
+    `,
+      components: {
+        Provider,
+        Tooltip,
+        TooltipTrigger,
+        TooltipContent,
+      },
+    })
+  );
+  await nextTick();
+
+  // Because the tooltip has aria-hidden="true", we need to use { hidden: true } to retrieve it.
+  expect(screen.getByRole("tooltip", { hidden: true })).toBeInTheDocument();
+});
+
+
+
 it("should provide description to the trigger when the tooltip is open", async () => {
   render(
     h({
