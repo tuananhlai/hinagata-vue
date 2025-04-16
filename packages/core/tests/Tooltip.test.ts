d0d36c9c4a5962ad2796d767e226a2fb29a15200
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2025-04-16 20:11:42 +0900

    minor changes

--- packages/core/tests/Tooltip.test.ts
+++ packages/core/tests/Tooltip.test.ts
@@ -1,6 +1,32 @@
-import { it, expect } from 'vitest';
-import { Tooltip } from '../src';
+import { render, screen } from "@testing-library/vue";
+import { expect, it } from "vitest";
+import { h, nextTick } from "vue";
+import { Provider, Tooltip, TooltipContent, TooltipTrigger } from "../src";
 
-it('should render', () => {
-  expect(true).toBe(true);
+it("should provide description to the trigger when the tooltip is open", async () => {
+  render(
+    h({
+      template: `
+    <Provider>
+      <Tooltip default-open>
+        <TooltipTrigger as-child>
+          <button>Hover me</button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Tooltip Content</p>
+        </TooltipContent>
+      </Tooltip>
+    </Provider>
+    `,
+      components: {
+        Provider,
+        Tooltip,
+        TooltipTrigger,
+        TooltipContent,
+      },
+    })
+  );
+  await nextTick();
+
+  expect(screen.getByRole('button')).toHaveAccessibleDescription('Tooltip Content');
 });
