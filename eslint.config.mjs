6b7ffbfe1bd10774c9391c955fdc9a86efb9646e
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2025-04-21 21:50:40 +0900

    minor changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,6 @@ import {
 } from "@vue/eslint-config-typescript";
 import pluginVue from "eslint-plugin-vue";
 
-// files: [
-//   "packages/core/src/**/*.{ts,vue}",
-//   "examples/dogfood/src/**/*.{ts,vue}",
-// ],
 export default defineConfigWithVueTs(
   pluginVue.configs["flat/essential"],
   vueTsConfigs.recommended,
