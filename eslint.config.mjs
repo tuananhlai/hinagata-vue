6ee54c094054dbbe6e6a78ff803d62ef4cfd71e1
tuananhlai <42168740+tuananhlai@users.noreply.github.com>
2025-04-21 09:09:09 +0900

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,15 @@
-import vue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
-// import path from "node:path";
-// import { fileURLToPath } from "node:url";
-// import js from "@eslint/js";
-// import { FlatCompat } from "@eslint/eslintrc";
+import {
+    defineConfigWithVueTs,
+    vueTsConfigs
+} from "@vue/eslint-config-typescript";
+import pluginVue from "eslint-plugin-vue";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const compat = new FlatCompat({
-//     baseDirectory: __dirname,
-//     recommendedConfig: js.configs.recommended,
-//     allConfig: js.configs.all
-// });
+// files: [
+//   "packages/core/src/**/*.{ts,vue}",
+//   "examples/dogfood/src/**/*.{ts,vue}",
+// ],
 
-export default defineConfig([
-  {
-    files: [
-      "packages/core/src/**/*.{ts,vue}",
-      "examples/dogfood/src/**/*.{ts,vue}",
-    ],
-    plugins: {
-      vue,
-    },
-    rules: {
-      "vue/multi-word-component-names": "off",
-    },
-  },
-]);
+export default defineConfigWithVueTs(
+  pluginVue.configs["flat/essential"],
+  vueTsConfigs.recommended
+);
